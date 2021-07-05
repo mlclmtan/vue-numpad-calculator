d043bc193346e35a971d9ae291740d4276bbbdb7
mlclmtan <mlclmtan@gmail.com>
2021-07-05 12:24:42 +0800

    sh deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,11 +15,12 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
+git checkout -b gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:mlclmtan/vue-numpad-calculator.git main:gh-pages
+git push -f git@github.com:mlclmtan/vue-numpad-calculator.git deploy:gh-pages
 
 cd -
