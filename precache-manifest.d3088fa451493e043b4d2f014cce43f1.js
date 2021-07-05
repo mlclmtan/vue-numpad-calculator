a76ae4b978c27773c418f610a98280f05eddd3d6
mlclmtan <mlclmtan@gmail.com>
2021-07-05 12:15:56 +0800

    deploy

---
+++ precache-manifest.d3088fa451493e043b4d2f014cce43f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7affc9c394c2934610",
+    "url": "/vue-numpad-calculator/css/app.d56a393f.css"
+  },
+  {
+    "revision": "6cf38bbc47797f14151e93708841cb1f",
+    "url": "/vue-numpad-calculator/index.html"
+  },
+  {
+    "revision": "ad7affc9c394c2934610",
+    "url": "/vue-numpad-calculator/js/app.754cee0a.js"
+  },
+  {
+    "revision": "4b996c4a2b3b3b502723",
+    "url": "/vue-numpad-calculator/js/chunk-vendors.ededcaf2.js"
+  },
+  {
+    "revision": "e733d5a65666dd17b4defc45a3234e9e",
+    "url": "/vue-numpad-calculator/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-numpad-calculator/robots.txt"
+  }
+]);
